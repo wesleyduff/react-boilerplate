709e9e27ba151aa87d36c4678751c7ef545177b4
Wes Duff <wduff@healthgrades.com>
2018-04-12 21:14:11 -0600

    Added Enzyme, jest config update w/ enzyme, hello world component, hello world component tested, npm scripts created for test and coverage, open coverage html hosting on npm script open:coverage

---
+++ client/src/components/HelloWorld/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const HelloWorld = () => {
+ 
+    return (
+        <div data-qatarget="hello-world-wrapper">
+            Hello World!
+        </div>
+    );
+};
+ 
+export default HelloWorld;
